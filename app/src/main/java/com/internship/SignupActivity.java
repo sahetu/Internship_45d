42d7409b48e1fe015a26cff7ad67509dc0ff77fa
Sagar Khatri <khatrisagar2@gmail.com>
2023-08-03 10:27:17 +0530

    3rd Aug Update

--- app/src/main/java/com/internship/SignupActivity.java
+++ app/src/main/java/com/internship/SignupActivity.java
@@ -2,23 +2,29 @@ package com.internship;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class SignupActivity extends AppCompatActivity {
 
     Button signup;
-    EditText name, email, contact, password, confirmPassword;
+    EditText name, email, contact, password, confirmPassword,dob;
     TextView login;
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
@@ -30,6 +36,9 @@ public class SignupActivity extends AppCompatActivity {
     //String[] cityArray = {"Ahmedabad","Gandhinagar","Vadodara","Surat","Rajkot","Patan","Modasa","Mehsana","Bharuch","Ahmedabad","Gandhinagar","Vadodara","Surat","Rajkot","Patan","Modasa","Mehsana","Bharuch"};
 
     ArrayList<String> arrayList;
+
+    Calendar calendar;
+    String sCity = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +53,33 @@ public class SignupActivity extends AppCompatActivity {
         confirmPassword = findViewById(R.id.signup_confirm_password);
 
         city = findViewById(R.id.signup_city);
+        dob = findViewById(R.id.signup_dob);
+
+        calendar = Calendar.getInstance();
+
+        DatePickerDialog.OnDateSetListener dateClick = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                calendar.set(Calendar.YEAR,i);
+                calendar.set(Calendar.MONTH,i1);
+                calendar.set(Calendar.DAY_OF_MONTH,i2);
+
+                SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+                dob.setText(sdf.format(calendar.getTime()));
+
+            }
+        };
+
+        dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(SignupActivity.this,dateClick,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
+                //datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+                datePickerDialog.show();
+
+            }
+        });
 
         arrayList = new ArrayList<>();
 
@@ -58,6 +94,7 @@ public class SignupActivity extends AppCompatActivity {
         arrayList.set(3,"Vadodara");
 
         arrayList.add(0,"Jamnagar");
+        arrayList.add(0,"Select City");
 
         ArrayAdapter adapter = new ArrayAdapter(SignupActivity.this, android.R.layout.simple_list_item_1,arrayList);
         adapter.setDropDownViewResource(android.R.layout.simple_list_item_checked);
@@ -66,7 +103,14 @@ public class SignupActivity extends AppCompatActivity {
         city.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                new CommonMethod(SignupActivity.this,arrayList.get(i));
+                if(i==0){
+                    sCity= "";
+                    Log.d("City","Selected City = "+sCity);
+                }
+                else{
+                    sCity = arrayList.get(i);
+                    new CommonMethod(SignupActivity.this,sCity);
+                }
             }
 
             @Override
@@ -135,8 +179,17 @@ public class SignupActivity extends AppCompatActivity {
                 else if(!confirmPassword.getText().toString().trim().matches(password.getText().toString())){
                     confirmPassword.setError("Password Does Not Match");
                 }
+                else if(gender.getCheckedRadioButtonId() == -1){
+                    new CommonMethod(SignupActivity.this,"Please Select Gender");
+                }
+                else if(sCity.equals("")){
+                    new CommonMethod(SignupActivity.this,"Please Select City");
+                }
+                else if(dob.getText().toString().trim().equals("")){
+                    dob.setError("Please Select Date Of Birth");
+                }
                 else {
-                    System.out.println("Login Successfully");
+                    System.out.println("Signup Successfully");
                     //Toast.makeText(MainActivity.this,"Login Successfully",Toast.LENGTH_LONG).show();
                     new CommonMethod(SignupActivity.this, "Signup Successfully");
                     //Snackbar.make(view, "Login Successfully", Snackbar.LENGTH_SHORT).show();
