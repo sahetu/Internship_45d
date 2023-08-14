5af122ba5646b529d64ca6481a4abb3e9c152671
Sagar Khatri <khatrisagar2@gmail.com>
2023-08-14 10:07:42 +0530

    14th Aug Update

--- app/src/main/java/com/internship/ProductDetailActivity.java
+++ app/src/main/java/com/internship/ProductDetailActivity.java
@@ -2,16 +2,25 @@ package com.internship;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class ProductDetailActivity extends AppCompatActivity {
+import com.razorpay.Checkout;
+import com.razorpay.PaymentData;
+import com.razorpay.PaymentResultWithDataListener;
 
-    TextView name,price,desc;
+import org.json.JSONObject;
+
+public class ProductDetailActivity extends AppCompatActivity implements PaymentResultWithDataListener {
+
+    TextView name, price, desc;
     ImageView imageView;
 
     Button buyNow;
@@ -23,7 +32,7 @@ public class ProductDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
 
-        sp = getSharedPreferences(ConstantSp.PREF,MODE_PRIVATE);
+        sp = getSharedPreferences(ConstantSp.PREF, MODE_PRIVATE);
 
         imageView = findViewById(R.id.product_detail_image);
         name = findViewById(R.id.product_detail_name);
@@ -32,19 +41,77 @@ public class ProductDetailActivity extends AppCompatActivity {
 
         buyNow = findViewById(R.id.product_detail_buy_now);
 
-        name.setText(sp.getString(ConstantSp.PRODUCT_NAME,""));
-        price.setText(ConstantSp.PRICE_SYMBOL+sp.getString(ConstantSp.PRODUCT_PRICE,""));
+        name.setText(sp.getString(ConstantSp.PRODUCT_NAME, ""));
+        price.setText(ConstantSp.PRICE_SYMBOL + sp.getString(ConstantSp.PRODUCT_PRICE, ""));
 
-        desc.setText(sp.getString(ConstantSp.PRODUCT_DESCRIPTION,""));
+        desc.setText(sp.getString(ConstantSp.PRODUCT_DESCRIPTION, ""));
 
-        imageView.setImageResource(Integer.parseInt(sp.getString(ConstantSp.PRODUCT_IMAGE,"")));
+        imageView.setImageResource(Integer.parseInt(sp.getString(ConstantSp.PRODUCT_IMAGE, "")));
+
+        Checkout.preload(getApplicationContext());
 
         buyNow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startPayment();
             }
         });
 
+    }
+
+    public void startPayment() {
+        /*
+          You need to pass current activity in order to let Razorpay create CheckoutActivity
+         */
+        final Activity activity = this;
+
+        final Checkout co = new Checkout();
+
+        co.setKeyID("rzp_test_xsiOz9lYtWKHgF");
+
+        try {
+            JSONObject options = new JSONObject();
+            options.put("name", getResources().getString(R.string.app_name));
+            options.put("description", "Online Order Purchase");
+            options.put("send_sms_hash", true);
+            options.put("allow_rotation", true);
+            //You can omit the image option to fetch the image from dashboard
+            options.put("image", "https://emojiisland.com/cdn/shop/products/Flushed_Face_Emoji_grande.png?v=1571606037");
+            options.put("currency", "INR");
+            options.put("amount", Integer.parseInt(sp.getString(ConstantSp.PRODUCT_PRICE, ""))*100);
+
+            JSONObject preFill = new JSONObject();
+            preFill.put("email", sp.getString(ConstantSp.EMAIL,""));
+            preFill.put("contact", sp.getString(ConstantSp.CONTACT,""));
+
+            options.put("prefill", preFill);
+
+            co.open(activity, options);
+        } catch (Exception e) {
+            Toast.makeText(activity, "Error in payment: " + e.getMessage(), Toast.LENGTH_SHORT)
+                    .show();
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onPaymentSuccess(String s, PaymentData paymentData) {
+        try {
+            Toast.makeText(this, "Payment Successful :\nPayment ID: " + s + "\nPayment Data: " + paymentData.getData(), Toast.LENGTH_SHORT).show();
+            Log.d("RESPONSE_SUCCESS","Payment Successful :\nPayment ID: " + s + "\nPayment Data: " + paymentData.getData());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onPaymentError(int i, String s, PaymentData paymentData) {
+        try {
+            Toast.makeText(this, "Payment Failed:\nPayment Data: " + paymentData.getData(), Toast.LENGTH_SHORT).show();
+            Log.d("RESPONSE_FAIL","Payment Failed:\nPayment Data: " + paymentData.getData());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
