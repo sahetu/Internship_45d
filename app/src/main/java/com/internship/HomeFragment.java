fa94a02b7ef341eb2cc2ff0970951f7ec2607980
Sagar Khatri <khatrisagar2@gmail.com>
2023-08-16 10:07:40 +0530

    16th Aug Update

--- app/src/main/java/com/internship/HomeFragment.java
+++ app/src/main/java/com/internship/HomeFragment.java
@@ -12,19 +12,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
 
     RecyclerView recyclerView;
 
-    String[] nameArray = {"Bread","Cloth","Makeup Kit"};
-    int[] imageArray = {R.drawable.bread,R.drawable.cloth,R.drawable.makup_kit};
+    String[] nameArray = {"Bread","Cloth","Makeup Kit","Bread","Cloth","Makeup Kit"};
+    int[] imageArray = {R.drawable.bread,R.drawable.cloth,R.drawable.makup_kit,R.drawable.bread,R.drawable.cloth,R.drawable.makup_kit};
 
-    String[] priceArray = {"50","2000","4000"};
+    String[] priceArray = {"50","2000","4000","50","2000","4000"};
     String[] descArray = {
+            "Bread, baked food product made of flour or meal that is moistened, kneaded, and sometimes fermented. A major food since prehistoric times, it has been made in various forms using a variety of ingredients and methods throughout the world.",
+            "Piece of cloth - a separate part consisting of fabric. piece of material. bib - top part of an apron; covering the chest. chamois cloth - a piece of chamois used for washing windows or cars. dishcloth, dishrag - a cloth for washing dishes.",
+            "What does a makeup kit consist of? Ideally, a complete makeup kit consists of a moisturizer, skin tint (like foundation or tinted primer), concealer, lip product, bronzer, blush, and mascara",
             "Bread, baked food product made of flour or meal that is moistened, kneaded, and sometimes fermented. A major food since prehistoric times, it has been made in various forms using a variety of ingredients and methods throughout the world.",
             "Piece of cloth - a separate part consisting of fabric. piece of material. bib - top part of an apron; covering the chest. chamois cloth - a piece of chamois used for washing windows or cars. dishcloth, dishrag - a cloth for washing dishes.",
             "What does a makeup kit consist of? Ideally, a complete makeup kit consists of a moisturizer, skin tint (like foundation or tinted primer), concealer, lip product, bronzer, blush, and mascara"
     };
+
+    RecyclerView categoryRecyclerview;
+    String[] categoryNameArray = {"Travel","Fashion","Health","Beauty","Food Drink"};
+    int[] categoryImageArray = {R.drawable.travel,R.drawable.fashion,R.drawable.health,R.drawable.beauty,R.drawable.food_drink};
+
+    ArrayList<CategoryList> arrayList;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -45,10 +56,25 @@ public class HomeFragment extends Fragment {
         //recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
 
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(false);
 
         ProductAdapter adapter = new ProductAdapter(getActivity(),nameArray,imageArray,priceArray,descArray);
         recyclerView.setAdapter(adapter);
 
+        categoryRecyclerview = view.findViewById(R.id.home_recyclerview_category);
+        categoryRecyclerview.setLayoutManager(new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.HORIZONTAL));
+        categoryRecyclerview.setItemAnimator(new DefaultItemAnimator());
+        categoryRecyclerview.setNestedScrollingEnabled(false);
+
+        arrayList = new ArrayList<>();
+        for(int i=0;i<categoryNameArray.length;i++){
+            CategoryList  list = new CategoryList();
+            list.setName(categoryNameArray[i]);
+            list.setImage(categoryImageArray[i]);
+            arrayList.add(list);
+        }
+        CategoryAdapter catAdapter = new CategoryAdapter(getActivity(),arrayList);
+        categoryRecyclerview.setAdapter(catAdapter);
         return view;
     }
 }
